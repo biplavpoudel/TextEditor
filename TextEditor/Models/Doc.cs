a2ab41d8379e436d3651eddb29a20359b44ee853
biplavpoudel <biplavpoudel764@gmail.com>
2024-09-08 19:38:56 +0545

    Add project files.

---
+++ TextEditor/Models/Doc.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TextEditor.Models;
+
+public class Doc
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    [Required]
+    public string? UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public IdentityUser? User { get; set; }
+}
